40ff59a82679305cf8f7986356a725aa81d36aba
Yuliia Skabitskaya <yulik777@i.ua>
2022-02-06 21:07:43 +0200

    change file

--- main.js
+++ main.js
@@ -1,44 +1,37 @@
-let now = new Date(); //строчка создает текущую дату
-console.log(now);
+function formatDate(date) {
+    let diff = new Date() - date;
 
-let timestamp = new Date(24 * 3600 * 1000); // сдвинуть дату назад (минуты * секунды * дни)
-console.log(timestamp);
+    if (diff < 1000) {
+        return 'прямо сейчас';
+    }
 
-let date = new Date("2021-01-26"); // еще одна строка создания даты
-console.log(date);
+    let sec = Math.floor(diff / 1000);
 
-let num = new Date(2021, 0, 26); // еще способ содания даты (тут месяцы с нуля начинаются)
-console.log(num);
+    if (sec < 60) {
+        return sec + ' сек. назад';
+    }
 
-console.log(num.getDate()); //вывести само число
-console.log(num.getMonth() + 1); //вывести месяц (начинается с нуля, поєтому дописываем +1)
-console.log(num.getFullYear()); //вывести само число
+    let min = Math.floor(diff / 60000);
+    if (min < 60) {
+        return min + ' мин. назад';
+    }
 
-console.log(`${num.getDate()}.${num.getMonth() + 1}.${num.getFullYear()}`); //сет вывода полной даты
+    let d = date;
+    d = [
+        '0' + d.getDate(),
+        '0' + (d.getMonth() + 1),
+        '' + d.getFullYear(),
+        '0' + d.getHours(),
+        '0' + d.getMinutes()
+    ].map(component => component.slice(-2));
 
-num.setFullYear(2022); //дата в другом формате
-console.log(num);
-
-let options = {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    timezone: 'UTC'
-};
-console.log(new Date().toLocaleString("ua",
-    options)); // автоматический вывод даты, но это не самый удобный вариант
-
-let diff = Date.now() - date; //(текущая дата - дату (в верхней фукции указана))
-console.log(diff / 1000 /
-    3600); // diff (timestap)- разница между датами в  милисикундах поделена на дни и минуты = сколько часов прошло
-
-
-let dat = new Date(2021, 0, 24, 22, 51);
-console.log(dat); // задать дату
-
-function getSecondsToTomorrow() {
-    let today = new Date();
-    console.log(today);
-    let tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
-    //console.log (tomorrow);
+    return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
 }
+
+alert(formatDate(new Date(new Date - 1)));
+
+alert(formatDate(new Date(new Date - 30 * 1000)));
+
+alert(formatDate(new Date(new Date - 5 * 60 * 1000)));
+
+alert(formatDate(new Date(new Date - 86400 * 1000)));
